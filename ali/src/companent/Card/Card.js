4f7720007c2de526783f98dcdbfd272be2af4b6d
Ali <141232212+AliKhaydarov@users.noreply.github.com>
2023-11-07 23:32:02 +0500

    Update Card.js

--- ali/src/companent/Card/Card.js
+++ ali/src/companent/Card/Card.js
@@ -21,7 +21,7 @@ function Cardi({tittle,image,price,description}) {
       <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src={image} />
         <Card.Body>
-          <Card.Title>allwih</Card.Title>
+          <Card.Title>allwiht</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
